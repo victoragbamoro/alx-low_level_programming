760359953190e878a0d6d96476fac1fcaa281004
Victor Agbamoro <agbamorovictor@gmail.com>
2022-03-12 06:29:48 -0800

    using write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Print word
+ * Return: 0
+ *
+ *
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 63);
+	return (1);
+}

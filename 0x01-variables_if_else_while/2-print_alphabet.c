d03bddb4d80255ea78b162cdc502ab290d04cec3
Victor Agbamoro <agbamorovictor@gmail.com>
2022-03-13 21:33:12 -0700

    writing a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Printing Alphabets in lower case
+ *
+ * Return:0
+ *
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchart(10);
+	return (0);
+}

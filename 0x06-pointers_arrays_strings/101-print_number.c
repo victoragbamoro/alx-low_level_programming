3514e9afd989b9761e3818bc20dad8121178ef44
Victor Agbamoro <agbamorovictor@gmail.com>
2022-03-26 07:43:42 -0700

    hello

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,43 +1,35 @@
 #include "main.h"
+
 /**
-* print_number - prints number
-* @n:integer to convert to character
-*Return: n
-*/
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
 void print_number(int n)
 {
-unsigned int abs;
-int mult = 1;
-unsigned int abSCount;
-int i;
-int c = 0;
+	unsigned int m, d, count;
 
-if (n == 0)
-{
-_putchar('0');
-}
-if (n < 0)
-{
-_putchar('-');
-n += 1;
-n *= -1;
-n++;
-}
-abs = n;
-abSCount = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
 
-while (abSCount > 0)
-{
-abSCount /= 10;
-c++;
-}
-for (i = 0; i < c - 1; i++)
-mult *= 10;
+	d = m;
+	count = 1;
 
-for (i = 0; i < c; i++)
-{
-_putchar((abs / mult) +'0');
-abs = abs % mult;
-mult /= 10;
-}
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifdef MAIN_H_
+#define MAIN_H_
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -5,6 +8,10 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
+char *cap_string(char *s);
+char *leet(char *s);
+char *rot13(char *s);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+#endif
